497a31fc025f3bd8b6dc16735df7a5480babbba6
Perrine Martyris <perrinemartyris@gmail.com>
2019-04-10 13:42:57 +0200

    add the history part of the homepage

---
+++ src/body/Body.jsx
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import History from './History.jsx';
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+      <History />
+      </div>
+    )
+  }
+}
+
+export default Body
